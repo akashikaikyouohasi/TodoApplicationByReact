0a4a5699408ebd779aedd9a6eb94e1c21972faf3
akashikaikyouohasi <hayatata@live.jp>
2021-07-11 10:35:47 +0900

    Material-UIを使用して装飾

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Select from "@material-ui/core/Select";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 import "./App.css";
 
 type Todo = {
@@ -16,7 +22,7 @@ const App: React.VFC = () => {
   const [filter, setFilter] = useState<Filter>("all"); //初期値はall
 
   const handleOnSubmit = (
-    e: React.FormEvent<HTMLFormElement | HTMLInputElement>
+    e: React.FormEvent<HTMLFormElement | HTMLInputElement | HTMLButtonElement>
   ) => {
     e.preventDefault();
     //no text
@@ -87,62 +93,78 @@ const App: React.VFC = () => {
 
   return (
     <div>
-      <select
-        defaultValue="all"
-        onChange={(e) => setFilter(e.target.value as Filter)} //asは型アサーション。変数の型を上書き
-      >
-        <option value="all">すべてのタスク</option>
-        <option value="checked">完了したタスク</option>
-        <option value="unchecked">未完了のタスク</option>
-        <option value="removed">削除済みのタスク</option>
-      </select>
-      {filter === "removed" ? (
-        <button
-          onClick={() => handleOnEmpty()}
-          disabled={todos.filter((todo) => todo.removed).length === 0}
-        >
-          ゴミ箱を空にする
-        </button>
-      ) : (
-        <form onSubmit={(e) => handleOnSubmit(e)}>
-          <input
-            type="text"
-            value={text}
-            disabled={filter === "checked"}
-            onChange={(e) => setText(e.target.value)}
-          />
-          <input
-            type="submit"
-            disabled={filter === "checked"}
-            value="追加"
-            onSubmit={(e) => handleOnSubmit(e)}
-          />
-        </form>
-      )}
-
-      <ul>
-        {filteredTodos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <input
-                type="checkbox"
-                disabled={todo.removed}
-                checked={todo.checked}
-                onChange={(e) => handleOnCheck(todo.id, todo.checked)}
-              />
-              <input
-                type="text"
-                disabled={todo.checked || todo.removed}
-                value={todo.value}
-                onChange={(e) => handleOnEdit(todo.id, e.target.value)}
-              />
-              <button onClick={() => handleOnRemove(todo.id, todo.removed)}>
-                {todo.removed ? "復元" : "削除"}
+      <Container maxWidth="sm">
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Select
+              native
+              defaultValue="all"
+              onChange={(e) => setFilter(e.target.value as Filter)} //asは型アサーション。変数の型を上書き
+            >
+              <option value="all">すべてのタスク</option>
+              <option value="checked">完了したタスク</option>
+              <option value="unchecked">未完了のタスク</option>
+              <option value="removed">削除済みのタスク</option>
+            </Select>
+            {filter === "removed" ? (
+              <button
+                onClick={() => handleOnEmpty()}
+                disabled={todos.filter((todo) => todo.removed).length === 0}
+              >
+                ゴミ箱を空にする
               </button>
-            </li>
-          );
-        })}
-      </ul>
+            ) : (
+              <form onSubmit={(e) => handleOnSubmit(e)}>
+                <TextField
+                  id="outlined-basic"
+                  variant="outlined"
+                  value={text}
+                  disabled={filter === "checked"}
+                  onChange={(e) => setText(e.target.value)}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={filter === "checked"}
+                  onSubmit={(e) => handleOnSubmit(e)}
+                >
+                  追加
+                </Button>
+              </form>
+            )}
+          </Grid>
+          <ul>
+            {filteredTodos.map((todo) => {
+              return (
+                <li key={todo.id}>
+                  <Checkbox
+                    disabled={todo.removed}
+                    checked={todo.checked}
+                    onChange={(e) => handleOnCheck(todo.id, todo.checked)}
+                    color="primary"
+                    inputProps={{ "aria-label": "primary  checkbox" }}
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    disabled={todo.checked || todo.removed}
+                    value={todo.value}
+                    onChange={(e) => handleOnEdit(todo.id, e.target.value)}
+                  />
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => handleOnRemove(todo.id, todo.removed)}
+                  >
+                    {todo.removed ? "復元" : "削除"}
+                  </Button>
+                </li>
+              );
+            })}
+          </ul>
+        </Grid>
+      </Container>
     </div>
   );
 };
